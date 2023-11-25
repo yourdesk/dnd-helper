fea28266316979660bf6ad51abc109d5f43b86be
Charles DiGiovanni <charles.digiov@gmail.com>
2023-11-25 17:21:31 -0500

    empty block styling

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-function createBlock(title) {
-    let section = document.createElement("section");
-    let mainGrid = document.getElementById("main-grid");
+let mainGrid = document.getElementById("main-grid");
 
-    let textElement = document.createElement("p");
+function newSection() {
+    return document.createElement("section");
+}
+
+function elem(type) {
+    return document.createElement(type);
+}
+
+function newBlock(title) {
+    let section = newSection();
+
+    let textElement = elem("p");
     textElement.innerText = title;
-    let input = document.createElement("input");
+    let input = elem("input");
     input.type = "text";
 
     section.appendChild(textElement);
     section.appendChild(input);
 
-    mainGrid.appendChild(section);
+    return section;
 }
+
+function newEmptyBlock() {
+    let section = newSection();
+    section.className = "empty";
+
+    return section;
+} 
 
 let blocks = "a b c d e f".split(" ");
 
 for (let block of blocks) {
-    createBlock(block);
+    let section = newBlock(block);
+    mainGrid.appendChild(section);
 }
